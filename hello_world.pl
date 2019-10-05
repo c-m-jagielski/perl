d94aca63211dd342ef361650573e6358fcc9d7f1
Chris Jagielski <c.jagielski@caci.com>
2019-10-05 12:04:24 -0400

    more lines for beginners

--- hello_world.pl
+++ hello_world.pl
@@ -3,3 +3,12 @@ use strict;
 use warnings;
  
 print "Hello world!\n";
+
+# This is a comment
+
+print "I can split
+lines like this, neat.\n";
+
+print "But I need to use the newline, which is annoying\n";
+
+
